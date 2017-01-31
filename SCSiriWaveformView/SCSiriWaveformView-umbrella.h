2390027df7b54299079e9fc831873f14d5ab60e9
Andrey Panchenko <asfdfdfd@gmail.com>
2017-01-31 18:44:25 +0300

    Carthage support.

---
+++ SCSiriWaveformView/SCSiriWaveformView-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCSiriWaveformView.
+FOUNDATION_EXPORT double SCSiriWaveformViewVersionNumber;
+
+//! Project version string for SCSiriWaveformView.
+FOUNDATION_EXPORT const unsigned char SCSiriWaveformViewVersionString[];
+
+#include <SCSiriWaveformView/SCSiriWaveformView.h>
